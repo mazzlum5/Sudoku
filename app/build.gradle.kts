056dceaa39dc1bad9bf0a652903ac0effd10ee62
mazlum <mazlum@hotmail.com>
2025-12-06 23:13:37 +0100

    feat: Integrate Google Mobile Ads SDK
    - Add the dependency for Google Play Services Ads version 23.1.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.google.android.gms:play-services-ads:23.1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
